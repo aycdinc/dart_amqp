ef9843530d9376d4dc30a8999295376240b4d46b
Apriscott <scott@kapelewski.com>
2020-07-04 15:38:14 +0200

    Added back frame size

--- lib/src/protocol/io/tuning_settings.dart
+++ lib/src/protocol/io/tuning_settings.dart
@@ -3,7 +3,7 @@ part of dart_amqp.protocol;
 class TuningSettings {
   int maxChannels = 0;
 
-  int maxFrameSize = 0;
+  int maxFrameSize = 131072;
 
   Duration heartbeatPeriod = const Duration(milliseconds: 10000);
 }
